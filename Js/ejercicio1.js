519827b5da50129c41a2c9e87e900e8d23cdb7c6
waltopo <eltopolino009@gmail.com>
2025-04-14 19:10:06 -0300

    ejercicio 1

--- Js/ejercicio1.js
+++
@@ -1,7 +0,0 @@
-const integrantes = "Coronel Walter Eduardo, Soto Guillermo Javier, Lobo Alexander Emanuel, Mamani Erick Rolando, Ortiz Lucas Maximiliano"; // Asignar nombres a la variable
-
-const mostrarMensaje = () => {
-    console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript.`);
-};
-
-mostrarMensaje(); // Llamar a la función para mostrar el mensaje
