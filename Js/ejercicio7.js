be811941b79fd3f1fa783ea7a7998b9ce1dc8eb3
MamaniErick <mamanierick64@gmail.com>
2025-04-15 22:16:37 -0300

    Ejercicio7

---
+++ Js/ejercicio7.js
@@ -0,0 +1,13 @@
+const nombres = ["mariano","pedro","lionel","kevin","matias","erick"];
+
+let maxLength = nombres[0].length;
+let nombreMaxLength = nombres[0];
+
+nombres.forEach( (x) => {
+    if(maxLength < x.length){
+        maxLength = x.length;
+        nombreMaxLength = x;
+    }
+});
+
+console.log("El nombre mas largo es "+ nombreMaxLength);
