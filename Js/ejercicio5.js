48515116598c37316ee89e533ad436d9c98e2886
lucasortiz512 <lucasortiz.lmo@gmail.com>
2025-04-19 17:59:47 -0300

    ResolucionEjercicio5

---
+++ Js/ejercicio5.js
@@ -0,0 +1,6 @@
+const horas = 2;
+const minutos = 45;
+const segundos = (horas * 3600) + (minutos * 60);
+console.log(`Horas: ${horas}`);
+console.log(`Minutos: ${minutos}`);
+console.log(`Total en segundos: ${segundos}`);
