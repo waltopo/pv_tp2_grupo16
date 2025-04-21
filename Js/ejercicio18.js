41d499f0cf054ecbf2678605d152f8f0785e46b1
waltopo <eltopolino009@gmail.com>
2025-04-20 22:39:06 -0300

    cr

--- Js/ejercicio18.js
+++
@@ -1,9 +0,0 @@
-// función para cambiar el texto del párrafo
-export const cambiarTexto = () => {
-    const parrafo = document.getElementById("texto");
-    parrafo.textContent = "El texto ha sido cambiado.";
-};
-
-// Capturando el evento clic del botón
-const boton = document.getElementById("boton");
-boton.addEventListener("click", cambiarTexto);
