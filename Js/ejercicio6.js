1d8262c7a97487fcbbb1902540456b00b1b70505
Javiersot0 <haruhamada2@gmail.com>
2025-05-09 20:16:16 -0300

    ejercicio 6

--- Js/ejercicio6.js
+++ Js/ejercicio6.js
@@ -1,4 +1,4 @@
-const edades = [10, 5, 18, 25, 40, 35, 28, 33]; //cambiar estos numeros para probar si esque sirve
+const edades = [10, 5, 15, 20, 40, 35, 25, 30]; //cambiar estos numeros para probar si esque sirve
 
 const calcularPromedioEdades = (array) => {
     const suma = array.reduce((acumulador, edad) => acumulador + edad, 0);
